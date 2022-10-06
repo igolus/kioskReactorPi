215d1b0a531ecc2148cb88d4bdd277a5fc8d7af2
igolus <igolus@gmail.com>
2022-10-06 17:29:49 +0200

    fix deploy web site command

--- programs/jsScripts/commandsListener/deployWebSiteCommand.js
+++ programs/jsScripts/commandsListener/deployWebSiteCommand.js
@@ -3,7 +3,9 @@ const {exec} = require("child_process");
 
 function execDeploy(url, callBack) {
     const command = "sudo rm -rf /var/www/html/* && wget \"" + url+ "\" -O siteContent.zip && " +
-        "sudo unzip siteContent.zip -d /var/www/html && rm siteContent.zip && cp ~/kioskReactor/programs/rocketRioskUtil.js /var/www/html"
+        "sudo unzip siteContent.zip -d /var/www/html && rm siteContent.zip && " +
+        "wget https://raw.githubusercontent.com/igolus/rocketKioskPi/master/programs/rocketRioskUtil.js &&" +
+        "cp rocketRioskUtil.js /var/www/html"
     try {
         exec(command, (error, stdout, stderr) => {
             if (error) {
