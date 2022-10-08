7379d727ee91ec4c81be4873d2ee662f24c7116d
igolus <igolus@gmail.com>
2022-10-08 17:42:09 +0200

    clean up

--- programs/jsScripts/dbUtil/versionUtil.js
+++ programs/jsScripts/dbUtil/versionUtil.js
@@ -11,42 +11,6 @@ const getVersion = async () => {
     return doc.exists && doc.data();
 }
 
-const PATCH = "patch";
-const MINOR = "minor";
-const MAJOR = "major";
-
-const updateVersionRelease = async (type) => {
-    const versionItem = await getVersion();
-    let versionValues = versionItem.version.split(".");
-    if (type === PATCH) {
-        versionValues[2] = parseInt(versionValues[2]) + 1;
-    }
-    else if (type === MINOR) {
-        versionValues[1] = parseInt(versionValues[1]) + 1;
-    }
-    else if (type === MAJOR) {
-        versionValues[0] = parseInt(versionValues[0]) + 1;
-    }
-
-    const newVersion = versionValues.join('.');
-    console.log("New version: " + newVersion)
-    await updateVersion({version: newVersion})
-}
-
-const updateVersion = async (data) => {
-    const doc = await fireBaseDb
-        .collection(versionCollection)
-        .doc(versionId)
-
-    await doc.set(data);
-    return data;
-}
-
 module.exports = {
     getVersion: getVersion,
-    updateVersion: updateVersion,
-    updateVersionRelease: updateVersionRelease,
-    PATCH: PATCH,
-    MINOR: MINOR,
-    MAJOR: MAJOR
 }
