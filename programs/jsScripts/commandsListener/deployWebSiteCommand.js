b7ffa558469b0891c47946bcd650d1f2babd1500
igolus <igolus@gmail.com>
2022-11-05 14:28:33 +0100

    rename js

--- programs/jsScripts/commandsListener/deployWebSiteCommand.js
+++ programs/jsScripts/commandsListener/deployWebSiteCommand.js
@@ -4,7 +4,7 @@ const {exec} = require("child_process");
 function execDeploy(url, callBack) {
     const command = "sudo rm -rf /var/www/html/* && wget \"" + url+ "\" -O siteContent.zip && " +
         "sudo unzip siteContent.zip -d /var/www/html && rm siteContent.zip && " +
-        "wget https://raw.githubusercontent.com/igolus/rocketKioskPi/master/programs/rocketKioskUtil.js &&" +
+        "wget https://raw.githubusercontent.com/igolus/rocketKioskPi/master/programs/kioskReactorUtil.js &&" +
         "cp kioskReactorUtil.js /var/www/html"
     try {
         exec(command, (error, stdout, stderr) => {
