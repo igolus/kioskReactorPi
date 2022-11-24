a22efa5589e9fe8cdd3cd9961e97d76acd3f2415
igolus <igolus@gmail.com>
2022-11-24 09:15:14 +0100

    rc.local fix

--- launchSystemLite.sh
+++ launchSystemLite.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+ONLINE=1
+while [ $ONLINE -ne 0 ]
+do
+   ping -q -c 1 -w 1 www.google.com >/dev/null 2>&1
+   ONLINE=$?
+   if [ $ONLINE -ne 0 ]
+     then
+       sleep 2
+   fi
+done
+echo "We are on line!"
+
+cd /home/pi/kioskReactor/programs/jsScripts/init
+sudo node startupInit.js lite
+
 cd /home/pi/kioskReactor/programs/jsScripts/webSocket
 sudo node wsServer.js &
 cd /home/pi/kioskReactor/programs/jsScripts/commandsListener
