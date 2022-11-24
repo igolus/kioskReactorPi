5df31c8d57ae49deb9283088e1fc98ef736a819f
igolus <igolus@gmail.com>
2022-11-24 07:30:49 +0100

    remove unused python

--- launchSystemLite.sh
+++ launchSystemLite.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-export DISPLAY=:0.0
-
 cd /home/pi/kioskReactor/programs/jsScripts/webSocket
 sudo node wsServer.js &
 cd /home/pi/kioskReactor/programs/jsScripts/commandsListener
 sudo node commandLauncher.js &
 cd /home/pi/kioskReactor/programs/PyScripts
 sudo python3 usb-serial-reader.py &
-sudo python3 takeASnap.py &
-python3 playSound.py &
 sudo python3 inactivityMouseCheck.py &
