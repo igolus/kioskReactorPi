f21bf61349273856f8b96b0ba019e2321a059e00
igolus <igolus@gmail.com>
2023-06-14 12:57:24 +0200

    Update

--- launchSystemWin.sh
+++ launchSystemWin.sh
@@ -68,6 +68,9 @@ minimalCreditToLock=-2000
 
 launchBrowser() {
 	wait-for-ws
+	echo $url
+	cd '/cygdrive/C/Program Files/Google/Chrome/Application'
+	./chrome.exe --no-sandbox --no-first-run --fast --fast-start --password-store=basic --disable-features=TouchpadOverscrollHistoryNavigation --disable-features=TranslateUI --noerrdialogs --incognito --disable-pinch --overscroll-history-navigation=0 --disable-infobars --remote-debugging-port=9222 $url &
 }
 
 cd /cygdrive/c/kioskReactor/programs/jsScripts/init
