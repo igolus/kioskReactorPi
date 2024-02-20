aa2e0005426be848e9ea11d57fb762e0de4f2786
Grousseau <igolus@gmail.com>
2024-02-20 15:00:07 +0100

    update

--- update.sh
+++ update.sh
@@ -8,4 +8,6 @@ ansible-pull -U https://github.com/igolus/rocketKioskPi.git local-windows.yml
 
 cd /cygdrive/c/kioskReactor/programs/jsScripts
 npm install --build-from-source --force
-shutdown /r
+chmod -R 777 /cygdrive/c/kioskReactor
+
+#shutdown /r
