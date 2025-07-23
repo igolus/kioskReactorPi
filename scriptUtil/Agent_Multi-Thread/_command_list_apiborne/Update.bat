d48fb6271067d01b9872790a10c72d5766a06d03
Grousseau <igolus@gmail.com>
2025-07-23 12:26:43 +0300

    Update update scripts

--- scriptUtil/Agent_Multi-Thread/_command_list_apiborne/Update.bat
+++ scriptUtil/Agent_Multi-Thread/_command_list_apiborne/Update.bat
@@ -2,3 +2,6 @@
 echo "Updating the system"
 
 REM update
+
+call C:\kioskReactor\Update.bat
+call C:\kioskReactor\Reboot.bat
--- update.sh
+++ update.sh
@@ -2,6 +2,11 @@
 ps -W | awk '/chrome.exe/,NF=1' | xargs kill -f
 ps -W | awk '/node.exe/,NF=1' | xargs kill -f
 ps -W | awk '/node/,NF=1' | xargs kill -f
+
+cd /cygdrive/c/kioskReactor/scriptUtil/Agent_Multi-Thread
+chmod +x ./killer.bat
+./killer.bat
+
 git config --global http.sslVerify false
 ansible-pull -U https://github.com/igolus/rocketKioskPi.git local-windows.yml
 #chmod 777 /cygdrive/c/kioskReactor/programs/jsScripts/cmdmp3.exe
@@ -10,4 +15,7 @@ cd /cygdrive/c/kioskReactor/programs/jsScripts
 npm install --build-from-source --force
 chmod -R 777 /cygdrive/c/kioskReactor
 
-#shutdown /r
+# Should be migrated to a proper c/kioskReactor/scriptUtil/_command_list_apiborne/Update.bat
+#cd /cygdrive/c/kioskReactor/scriptUtil/_command_list_apiborne
+#chmod +x ./Reboot.bat
+#./Reboot.bat
