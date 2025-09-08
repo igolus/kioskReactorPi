65498e99dd72b45db40f2c79c77a1a7e3cd3e6a3
nicolas.r <nicolas.rastorgueff@logicsante.com>
2025-09-08 14:13:50 +0200

    correction du fichier update.bat

--- update.bat
+++ update.bat
@@ -2,5 +2,7 @@
 call c:\cygwin64\bin\bash.exe C:\kioskReactor\update.sh
 
 REM Installation des services pour demarrage automatique avec Windows
+cd c:\kioskReactorPi\scriptUtil\Agent_Multi-Thread\
 call c:\kioskReactorPi\scriptUtil\Agent_Multi-Thread\service-install.bat
+cd c:\kioskReactorPi\scriptUtil\Camera\
 call c:\kioskReactorPi\scriptUtil\Camera\service-install.bat
