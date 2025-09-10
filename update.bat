5f82354d66d80151e2ada8f942ee92aae9ea7ea7
Grousseau <igolus@gmail.com>
2025-09-10 16:04:46 +0200

    fix bad folder

--- update.bat
+++ update.bat
@@ -5,16 +5,18 @@ call c:\cygwin64\bin\bash.exe C:\kioskReactor\update.sh
 
 echo update camera
 
-cd c:\kioskReactorPi\scriptUtil\Camera\
-call c:\kioskReactorPi\scriptUtil\Camera\service-install.bat
+cd c:\kioskReactor\scriptUtil\Camera\
+call c:\kioskReactor\scriptUtil\Camera\service-install.bat
 
 REM Installation des services pour demarrage automatique avec Windows
 echo Update Agent_Multi-Thread...
-cd c:\kioskReactorPi\scriptUtil\Agent_Multi-Thread\
-call c:\kioskReactorPi\scriptUtil\Agent_Multi-Thread\service-install.bat
+cd c:\kioskReactor\scriptUtil\Agent_Multi-Thread\
+call c:\kioskReactor\scriptUtil\Agent_Multi-Thread\service-install.bat
 
 :: Vérifie si l'argument "reboot" est passé
 IF /I "%1"=="reboot" (
+
     echo Redémarrage de la machine...
+    powershell -Command "Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::MsgBox('Mise à jour terminée. Le système va redémarrer dans 5 secondes.', 'OKOnly,Information', 'Mise à jour')"
     shutdown /r /t 5
 )
