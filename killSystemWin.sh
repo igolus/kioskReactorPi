eb75af3038ab99acbfa83e067b9615f0bba176e5
Guillaume Rousseau <igolus@gmail.com>
2026-01-30 15:24:00 +0100

    Kill system update

--- killSystemWin.sh
+++ killSystemWin.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Kill bash processes first (to stop run_service loops from restarting services)
+ps -W | awk '/bash.exe/,NF=1' | xargs kill -f
+
+# Then kill the services
 ps -W | awk '/chrome.exe/,NF=1' | xargs kill -f
 ps -W | awk '/node.exe/,NF=1' | xargs kill -f
 ps -W | awk '/node/,NF=1' | xargs kill -f
